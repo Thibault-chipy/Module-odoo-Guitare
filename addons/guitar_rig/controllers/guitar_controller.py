07fc5c03d063ccc34c6b9225ecee81d8ec1cdc4d
Thibault Chipy <chipythibault@gmail.com>
2025-12-30 05:33:09 +0100

    ajout photos matos + route fix

--- addons/guitar_rig/controllers/guitar_controller.py
+++ addons/guitar_rig/controllers/guitar_controller.py
@@ -1,9 +1,9 @@
 from odoo import http
 class Guitar(http.Controller):
-    @http.route('/guitarRig/allguitars', type='http', auth='public', website=True)
+    @http.route('/guitarRig/allgear', type='http', auth='public', website=True)
 
     def list(self, **kw):
-        guitars = http.request.env['guitar_rig.gear'].search([])
+        gears = http.request.env['guitar_rig.gear'].search([])
         return http.request.render('guitar_rig.list_gear_template', {
-            'gears': guitars
+            'gears': gears
         })
